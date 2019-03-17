e531c67e086ed839df2f2a5fe82cd597f26f467a
Anderson <andersonfcanel@gmail.com>
2019-03-17 11:01:25 -0300

    Configurando o Hibernate Util

---
+++ Drogaria/src/main/java/br/com/drogaria/HibernateUtil.java
@@ -0,0 +1,44 @@
+package br.com.drogaria;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+	private static SessionFactory fabricaDeSessoes = criarFabricaDeSessoes();
+
+	public static SessionFactory getFabricaDeSessoes() {
+		return fabricaDeSessoes;
+	}
+
+	/*
+	 * Atenção aos imports!
+	 * 
+	 */
+	private static SessionFactory criarFabricaDeSessoes() {
+		try {
+
+			/*
+			 * A linha abaixo busca o arquivo hibernate.cfg.xml e retorna suas
+			 * configurações.
+			 */
+			Configuration configuracao = new Configuration().configure();
+
+			/* A linha abaixo aplica as conficurações. */
+			ServiceRegistry registro = new StandardServiceRegistryBuilder().applySettings(configuracao.getProperties())
+					.build();
+
+			/* A linha abaixo utiliza o registro do serviço para criar a fábrica sessoes. */
+			SessionFactory fabrica = configuracao.buildSessionFactory(registro);
+			
+			//Retorna fábrica de sessoões.
+			return fabrica;
+		} catch (Throwable ex) {
+			System.err.println("A fábrica de sessões não pode ser criada." + ex);
+
+			// throw garante que a aplicação será avisada sobre a ocorrência da exception.
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+}
