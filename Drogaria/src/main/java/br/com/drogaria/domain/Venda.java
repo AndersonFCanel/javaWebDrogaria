c773e6e9dbb7c9ae4e9d000693476c906d8331a3
Anderson <andersonfcanel@gmail.com>
2019-03-17 15:04:04 -0300

    Venda - Relacionamento tipo Agregação

---
+++ Drogaria/src/main/java/br/com/drogaria/domain/Venda.java
@@ -0,0 +1,74 @@
+package br.com.drogaria.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * Classe para entidade Venda.
+ * 
+ * @author Anderson Canel
+ * @version 1.00
+ * @since Release 01 da aplicação
+ */
+//Observações:
+//@Temporal(TemporalType.TIMESTAMP) : Data e hora
+//Esta Entidade possui duas FK's
+//No caso do cliente é uma Agregação por isso aceita null
+
+@SuppressWarnings("serial")
+@Entity
+public class Venda extends GenericDomain {
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date horario;
+	
+	@Column(nullable = false, precision = 7, scale = 2)
+	private BigDecimal precoTotal;
+	
+	@ManyToOne
+	//@JoinColumn(nullable = false) 
+	private Cliente cliente;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Funcionario funcionario;
+
+	public Date getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+
+	public BigDecimal getPrecoTotal() {
+		return precoTotal;
+	}
+
+	public void setPrecoTotal(BigDecimal precoTotal) {
+		this.precoTotal = precoTotal;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+}
