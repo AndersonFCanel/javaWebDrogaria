2bed1a47f4c5cf9826b18f5b5d4c83fb653d0c77
Anderson <andersonfcanel@gmail.com>
2019-03-17 11:19:30 -0300

    JUnit Configurações Iniciais

---
+++ Drogaria/src/test/java/br/com/drogaria/HibernateUtilTest.java
@@ -0,0 +1,18 @@
+package br.com.drogaria;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.Test;
+
+public class HibernateUtilTest {
+	@Test
+	public void conectar() {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		sessao.close();
+
+		HibernateUtil.getFabricaDeSessoes().close();
+	}
+}
