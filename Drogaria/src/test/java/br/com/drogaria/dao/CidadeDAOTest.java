d1989a579ed14e83aadbe88bc927143e9dc5bf96
Anderson <andersonfcanel@gmail.com>
2019-03-20 23:32:21 -0300

    Alteracoes no gitignore

--- Drogaria/src/test/java/br/com/drogaria/dao/CidadeDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/CidadeDAOTest.java
@@ -1,17 +1,24 @@
 package br.com.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.drogaria.domain.Cidade;
 import br.com.drogaria.domain.Estado;
 
+
+//Observações:
+// No caso de trabalhar com FK, essa deve ser buscada, para que se passe um
+// objeto estado em cidade.setEstado(estado);
+
 public class CidadeDAOTest {
 	@Test
+	@Ignore
 	public void salvar() {
 		EstadoDAO estadoDAO = new EstadoDAO();
 
-		// No caso de trabalhar com FK, essa deve ser buscada, para que se passe um
-		// objeto estado em cidade.setEstado(estado);
 		Estado estado = estadoDAO.buscar(1L);
 
 		Cidade cidade = new Cidade();
@@ -20,5 +27,36 @@ public class CidadeDAOTest {
 
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		cidadeDAO.salvar(cidade);
+	}
+
+	@Test
+	@Ignore
+	public void listar() {
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		List<Cidade> resultado = cidadeDAO.listar();
+
+		for (Cidade cidade : resultado) {
+			System.out.println("Código da Cidade: " + cidade.getCodigo());
+			System.out.println("Nome da Cidade: " + cidade.getNome());
+			System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
+			System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+			System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+			System.out.println();
+		}
+	}
+
+	@Test
+	@Ignore
+	public void buscar() {
+		Long codigo = 7L;
+
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigo);
+
+		System.out.println("Código da Cidade: " + cidade.getCodigo());
+		System.out.println("Nome da Cidade: " + cidade.getNome());
+		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
+		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+		System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
 	}
 }
